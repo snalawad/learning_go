ae93eb316597e8e78b96fcd90817e0a85e43b555
Suyash Nalawade <sunalawa@redhat.com>
2024-08-05 15:20:31 +0530

    basic math libs

---
+++ 5_math/math.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+
+	i1, i2, i3 := 12, 54, 23
+	intSum := i1 + i2 + i3
+
+	fmt.Println("Integer Sum: ", intSum)
+
+	f1, f2, f3 := 23.4, 45.3, 45.2
+	floatSum := f1 + f2 + f3
+	fmt.Println("Float Sum:", floatSum)
+
+	floatSum = math.Round(floatSum*100) / 100
+	fmt.Println("Float Sum rounded up:", floatSum)
+
+}
